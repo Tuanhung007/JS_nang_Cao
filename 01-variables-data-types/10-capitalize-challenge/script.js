e152211f10b955b56819e0777603c677d483f985
Tuanhung007 <hunghtph37743@fpt.edu.vn>
2024-01-20 09:47:38 +0700

    luyentap_B2

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++
@@ -1,9 +0,0 @@
-const myString = 'developer';
-
-let myNewString;
-
-myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
-myNewString = myString[0].toUpperCase() + myString.substring(1);
-myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
-
-console.log(myNewString);
